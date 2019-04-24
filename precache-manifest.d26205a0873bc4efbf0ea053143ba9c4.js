9655b771b54521c641bfc30f2bb0a13c3333f87c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-24 00:20:50 +0000

    Deploy ronaldoaburto/talent-land-2019-workshop-frontend to github.com/ronaldoaburto/talent-land-2019-workshop-frontend.git:gh-pages

---
+++ precache-manifest.d26205a0873bc4efbf0ea053143ba9c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "351a26bc79f508a09000",
+    "url": "/static/js/main.f0ac276b.chunk.js"
+  },
+  {
+    "revision": "ed2eb8a31b3d3fe961a8",
+    "url": "/static/js/2.1efa75b7.chunk.js"
+  },
+  {
+    "revision": "351a26bc79f508a09000",
+    "url": "/static/css/main.71180b94.chunk.css"
+  },
+  {
+    "revision": "b514f2e93708e70f95d4ab4323d9b677",
+    "url": "/index.html"
+  }
+];
